a804331c901b45155ff64c56e428e74dcfa740dc
Vitalii-fedorov <fva23322@gmail.com>
2023-10-26 16:51:33 +0300

    now you can add post on profile page

--- src/Comps/Posts/Posts.jsx
+++ src/Comps/Posts/Posts.jsx
@@ -1,8 +1,9 @@
 import './Posts.css';
+import React, { useState } from 'react';
 
 const postData = [
     {id: 1, postText: 'Fuck You!'},
-    {id: 2, postText: 'Bitch'},
+
 ]
 
 const PostItem = (props) => {
@@ -14,15 +15,36 @@ const PostItem = (props) => {
 }
 
 const Posts = () => {
+
     let postsElements = postData.map ((el, index) =>
         <PostItem key={index} postAddedText={el.postText} id={el.id} />)
+
+    let addpost = () => {
+        if (newPostText.trim() !== '') { // Check if the input is not empty
+            const newPost = {
+                id: postData.length + 1,
+                postText: newPostText,
+            };
+
+            postData.push(newPost); // Step 3: Add the new object to the postData array
+            setNewPostText(''); // Step 4: Clear the textarea
+        }
+    }
+
+    const [newPostText, setNewPostText] = useState(''); // Step 1
+
     return (
         <div className="Posts">
 
             <span>My posts</span> <br />
             {postsElements}
-            <textarea placeholder="your news..."></textarea>
-            <div className="btn"><button><b>Send</b></button></div>
+            <textarea placeholder="your news..." id="new-post"  value={newPostText}
+                      onChange={(e) => setNewPostText(e.target.value)}
+                        ></textarea>
+            <div className="btn"><button
+                onClick={addpost}>
+                <b>Send</b>
+            </button></div>
 
         </div>
     );
